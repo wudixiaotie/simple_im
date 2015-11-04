3e34aa402799a3d5f59052dd927dbda4d489c0af
xiaotie <wudixiaotie@gmail.com>
2015-11-04 18:05:27 +0800

    update

--- gacp.sh
+++ gacp.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
 if [ -n "$1" ] ;then
-  note=$1
+  branch=$1
+else
+  branch="master"
+fi
+
+if [ -n "$2" ] ;then
+  note=$2
 else
   note="update"
 fi
 
 rm -rf erl_crash.dump
+git checkout $branch
 git add -A
 git commit -m "$note"
-git push -u origin
+git push -u origin $branch
