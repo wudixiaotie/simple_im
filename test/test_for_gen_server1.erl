230071cd9a64f9af0a9e8b7d8b4df5cf63a8b40d
xiaotie <wudixiaotie@gmail.com>
2016-04-07 14:43:39 +0800

    update test

--- test/test_for_gen_server1.erl
+++ test/test_for_gen_server1.erl
@@ -1,171 +1,146 @@
 -module (test_for_gen_server1).
 
--behaviour (gen_server).
+-export ([test1/1, test2/1, test3/1, test4/1, test5/1, test6/1]).
 
-%% APIs
--export([test1/1, test2/1, test3/1, test4/1, test5/1, test6/1, test7/1,
-         test8/1, test9/1]).
+-record (state, {timestamp, times}).
 
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+send_msg(0) ->
+    self() ! stop;
+send_msg(N) ->
+    self() ! a,
+    send_msg(N-1).
 
+% test for running function without GC
 test1(N) ->
-    {ok, Pid} = gen_server:start_link(?MODULE, [], []),
-    Pid ! {start, N}.
+    Pid = spawn(fun() -> loop1(undefined, 0) end), Pid ! {start, N}, ok.
 
-test2(N) -> Pid = spawn(fun() -> loop2([undefined, 0]) end), Pid ! {start, N}.
-test3(N) -> Pid = spawn(fun() -> loop3([undefined, 0]) end), Pid ! {start, N}.
-test4(N) -> Pid = spawn(fun() -> loop4([undefined, 0]) end), Pid ! {start, N}.
-test5(N) -> Pid = spawn(fun() -> loop5([undefined, 0]) end), Pid ! {start, N}.
-test6(N) -> Pid = spawn(fun() -> loop6([undefined, 0]) end), Pid ! {start, N}.
-test7(N) -> Pid = spawn(fun() -> loop7([undefined, 0]) end), Pid ! {start, N}.
-test8(N) -> Pid = spawn(fun() -> loop8(undefined, 0) end), Pid ! {start, N}.
-test9(N) -> Pid = spawn(fun() -> loop9({undefined, 0}) end), Pid ! {start, N}.
+loop1(Timestamp, Times) ->
+    receive
+        {start, N} ->
+            send_msg(N),
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            loop1(NTimestamp, 0);
+        stop ->
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            Cost = NTimestamp - Timestamp,
+            io:format("======Times:~p Cost:~p~n", [Times, Cost]);
+        _Info ->
+            loop1(Timestamp, Times + 1)
+    end.
 
-%%==============================================================================
 
-init([]) ->
-    {ok, []}.
-handle_call(_Request, _From, State) ->
-    {reply, nomatch, State}.
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-handle_info({start, N}, _State) ->
-    do_test(N),
-    {A,B,C} = os:timestamp(),
-    Timestamp = (A * 1000000 + B) * 1000000 + C,
-    {noreply, [Timestamp, 0]};
-handle_info(stop, [Timestamp, Times]) ->
-    {A,B,C} = os:timestamp(),
-    Timestamp1 = (A * 1000000 + B) * 1000000 + C,
-    Cost = Timestamp1 - Timestamp,
-    io:format("======gen_server:  Times:~p Cost:~p~n", [Times, Cost]),
-    {stop, normal, []};
-handle_info(_Info, [Timestamp, Times]) ->
-    {noreply, [Timestamp, Times + 1]}.
-
-terminate(_Reason, _State) -> ok.
-
-code_change(_OldVer, State, _Extra) -> {ok, State}.
-
-do_test(0) -> self() ! stop;
-do_test(N) -> self() ! a, do_test(N - 1).
-
-%%==============================================================================
+% test for running function with record GC
+test2(N) ->
+    Pid = spawn(fun() -> loop2(#state{timestamp = undefined, times = 0}) end), Pid ! {start, N}, ok.
 
 loop2(State) ->
-    Msg = receive
-              Input -> Input
-          end,
-    Reply = {ok, handle_info(Msg, State)},
-    handle_common_reply(Reply, Msg, State).
-
-handle_common_reply(Reply, _Msg, _State) ->
-    case Reply of
-        {ok, {noreply, NState}} -> loop2(NState);
-        {ok, {stop, normal, _}} -> ok
+    receive
+        {start, N} ->
+            send_msg(N),
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            NewState = #state{timestamp = NTimestamp, times = 0},
+            loop2(NewState);
+        stop ->
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            Cost = NTimestamp - State#state.timestamp,
+            io:format("======Times:~p Cost:~p~n", [State#state.times, Cost]);
+        _Info ->
+            NewState = State#state{times = State#state.times + 1},
+            loop2(NewState)
     end.
 
-%%==============================================================================
+
+% test for running function with map GC
+test3(N) ->
+    Pid = spawn(fun() -> loop3(#{timestamp => undefined, times => 0}) end), Pid ! {start, N}, ok.
 
 loop3(State) ->
-    Msg = receive
-              Input -> Input
-          end,
-    Reply = {ok, handle_info(Msg, State)},
-    case Reply of
-        {ok, {noreply, NState}} -> loop3(NState);
-        {ok, {stop, normal, _}} -> ok
+    receive
+        {start, N} ->
+            send_msg(N),
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            NewState = State#{timestamp => NTimestamp},
+            loop3(NewState);
+        stop ->
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            #{timestamp := Timestamp, times := Times} = State,
+            Cost = NTimestamp - Timestamp,
+            io:format("======Times:~p Cost:~p~n", [Times, Cost]);
+        _Info ->
+            #{times := Times} = State,
+            NewState = State#{times => Times + 1},
+            loop3(NewState)
     end.
 
-%%==============================================================================
+
+% test for running function with map GC
+test4(N) ->
+    Pid = spawn(fun() -> loop4(#{timestamp => undefined, times => 0}) end), Pid ! {start, N}, ok.
 
 loop4(State) ->
-    Msg = receive
-              Input -> Input
-          end,
-    case handle_info(Msg, State) of
-        {noreply, NState} -> loop4(NState);
-        {stop, normal, _} -> ok
-    end.
-
-%%==============================================================================
-
-loop5(State) ->
-    receive
-        Input ->
-            case handle_info(Input, State) of
-                {noreply, NState} -> loop5(NState);
-                {stop, normal, _} -> ok
-            end
-    end.
-
-%%==============================================================================
-
-loop6(State) ->
-    receive
-        {start, _N} = Msg ->
-            {noreply, NState} = handle_info(Msg, State),
-            loop6(NState);
-        stop = Msg ->
-            {stop, normal, []} = handle_info(Msg, State);
-        Info ->
-            {noreply, NState} = handle_info(Info, State),
-            loop6(NState)
-    end.
-
-%%==============================================================================
-
-loop7([Timestamp, Times]) ->
     receive
         {start, N} ->
-            do_test(N),
+            send_msg(N),
             {A,B,C} = os:timestamp(),
             NTimestamp = (A * 1000000 + B) * 1000000 + C,
-            loop7([NTimestamp, 0]);
+            NewState = State#{timestamp => NTimestamp},
+            loop4(NewState);
+        stop ->
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            #{timestamp := Timestamp, times := Times} = State,
+            Cost = NTimestamp - Timestamp,
+            io:format("======Times:~p Cost:~p~n", [Times, Cost]);
+        _Info ->
+            {ok, Times} = maps:find(times, State),
+            NewState = State#{times => Times + 1},
+            loop4(NewState)
+    end.
+
+
+% test for running function with list GC
+test5(N) ->
+    Pid = spawn(fun() -> loop5([undefined, 0]) end), Pid ! {start, N}, ok.
+
+loop5([Timestamp, Times]) ->
+    receive
+        {start, N} ->
+            send_msg(N),
+            {A,B,C} = os:timestamp(),
+            NTimestamp = (A * 1000000 + B) * 1000000 + C,
+            loop5([NTimestamp, Times]);
         stop ->
             {A,B,C} = os:timestamp(),
             NTimestamp = (A * 1000000 + B) * 1000000 + C,
             Cost = NTimestamp - Timestamp,
             io:format("======Times:~p Cost:~p~n", [Times, Cost]);
         _Info ->
-            loop7([Timestamp, Times + 1])
+            loop5([Timestamp, Times + 1])
     end.
 
-%%==============================================================================
 
-loop8(Timestamp, Times) ->
+% test for running function with tuple GC
+test6(N) ->
+    Pid = spawn(fun() -> loop6({undefined, 0}) end), Pid ! {start, N}, ok.
+
+loop6({Timestamp, Times}) ->
     receive
         {start, N} ->
-            do_test(N),
+            send_msg(N),
             {A,B,C} = os:timestamp(),
             NTimestamp = (A * 1000000 + B) * 1000000 + C,
-            loop8(NTimestamp, 0);
+            loop6({NTimestamp, Times});
         stop ->
             {A,B,C} = os:timestamp(),
             NTimestamp = (A * 1000000 + B) * 1000000 + C,
             Cost = NTimestamp - Timestamp,
             io:format("======Times:~p Cost:~p~n", [Times, Cost]);
         _Info ->
-            loop8(Timestamp, Times + 1)
-    end.
-
-%%==============================================================================
-
-loop9({Timestamp, Times}) ->
-    receive
-        {start, N} ->
-            do_test(N),
-            {A,B,C} = os:timestamp(),
-            NTimestamp = (A * 1000000 + B) * 1000000 + C,
-            loop9({NTimestamp, 0});
-        stop ->
-            {A,B,C} = os:timestamp(),
-            NTimestamp = (A * 1000000 + B) * 1000000 + C,
-            Cost = NTimestamp - Timestamp,
-            io:format("======Times:~p Cost:~p~n", [Times, Cost]);
-        _Info ->
-            loop9({Timestamp, Times + 1})
+            loop6({Timestamp, Times + 1})
     end.
